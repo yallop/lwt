f8bf87f4b43b3c8bfba18d7df1466f36aa949c65
Anton Bachin <antonbachin@yahoo.com>
2016-05-20 11:49:31 -0500

    Rename AppVeyor variables as in ocaml-ci-scripts.

--- utils/appveyor-cygwin-build.bat
+++ utils/appveyor-cygwin-build.bat
@@ -1,2 +1,2 @@
-%CYGSH% "ocamlc -version"
-%CYGSH% "cd /cygdrive/c/projects/lwt && opam pin add -yn . && opam install -ytb lwt"
+%CYG_BASH% "ocamlc -version"
+%CYG_BASH% "cd /cygdrive/c/projects/lwt && opam pin add -yn . && opam install -ytb lwt"
